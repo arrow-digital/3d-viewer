d21c8cddcf11dba5ddffccd9459175628eeb5ebd
Diego Ramos <diegorbrito9@gmail.com>
2024-06-27 13:23:04 -0300

    feat: render 3d model

--- src/components/scene/index.tsx
+++ src/components/scene/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls, STLLoader } from 'three/examples/jsm/Addons.js';
@@ -11,17 +12,13 @@ function setup(containerElement: HTMLElement) {
     return;
   }
 
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  scene.background = new THREE.Color('black');
+  scene.background = new THREE.Color('white');
 
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(1500, 600);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
   new OrbitControls(camera, renderer.domElement);
   containerElement.appendChild(renderer.domElement);
 
@@ -37,30 +34,55 @@ function setup(containerElement: HTMLElement) {
 
   function animate() {
     requestAnimationFrame(animate);
-
     renderer.render(scene, camera);
   }
 
   animate();
 }
 
+async function readStreamToArrayBuffer(stream: ReadableStream) {
+  const reader = stream.getReader();
+  const chunks = [];
+
+  let done = false;
+  while (!done) {
+    const { value, done: streamDone } = await reader.read();
+    if (value) {
+      chunks.push(value);
+    }
+    done = streamDone;
+  }
+
+  const arrayBuffer = new Uint8Array(chunks.reduce((acc, val) => acc.concat(Array.from(val)), []));
+  return arrayBuffer.buffer;
+}
+
 export function Scene() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const params = useSearchParams();
+  const modelUrl = params.get('url');
 
   useEffect(() => {
-    let loader = new STLLoader();
+    async function start() {
+      const url = modelUrl
+        ? modelUrl
+        : 'https://linkio-dev-bucket.s3.sa-east-1.amazonaws.com/orders/667add660c806be4e1b0f0fe/upperjaw_ff1990d4-1544-4208-84e7-aac74e669c22.stl';
 
-    loader.load('/upperjaw.stl', (model) => {
-      const object = new THREE.Mesh(
-        model,
-        new THREE.MeshLambertMaterial({ color: 'red' })
-      );
+      const res = await fetch(`/api/3d-model?url=${url}`);
+      const arrayBuffer = await readStreamToArrayBuffer(res.body!);
+
+      const loader = new STLLoader();
+
+      const geometry = loader.parse(arrayBuffer);
+      const material = new THREE.MeshLambertMaterial({ color: 'red' });
+      const object = new THREE.Mesh(geometry, material);
 
       scene.add(object);
-
       setup(containerRef.current!);
-    });
-  }, []);
+    }
+
+    start();
+  }, [modelUrl]);
 
   return <div ref={containerRef} />;
 }

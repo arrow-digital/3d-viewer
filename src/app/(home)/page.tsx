7e1b345e5cada09f36249417fbeee059dfc3c435
Diego Ramos <diegorbrito9@gmail.com>
2024-06-27 14:29:02 -0300

    chore: create styles using tailwind

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,16 @@
-'use client';
+import { Scene } from '@/components/scene';
+import { Metadata } from 'next';
 
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
 
-export default function Home() {
-  useEffect(() => {
-    redirect('/model-3d');
-  }, []);
+export const metadata: Metadata = { 
+  title: 'Visualizador 3d' 
+};
 
-  return <></>;
+
+export default function STLModel() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-black min-w-screen">
+      <Scene />
+    </main>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
-import type { Metadata } from 'next';
 import './globals.css';
 
-export const metadata: Metadata = { title: 'Create Next App' };
-
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>{children}</body>
     </html>
   );

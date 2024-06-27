22870054d2f2f51d2d7126ef9e670c51dda30b11
Diego Ramos <diegorbrito9@gmail.com>
2024-06-27 15:51:13 -0300

    chore: adding suspense api

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import { ProgressBar } from '@/components/progressBar';
 import { Scene } from '@/components/scene';
+import { Suspense } from 'react';
 
 export default function STLModel() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-black min-w-screen">
-      <ProgressBar />
-      <Scene />
+      <Suspense fallback={<ProgressBar />}>
+        <Scene />
+      </Suspense>
     </main>
   );
 }

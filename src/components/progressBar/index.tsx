5482035b81d0d58f7aac4424c599a60f70ea1750
Diego Ramos <diegorbrito9@gmail.com>
2024-06-28 16:46:59 -0300

    fix: styles

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -20,21 +20,23 @@ export function ProgressBar() {
       {true && (
         <section className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
           <div className="flex flex-col w-1/2 gap-4 items-center">
-            <h1 className="text-4xl text-white font-bold">Carregando ...</h1>
+            <h1 className="text-xl text-white font-bold self-start">Carregando ...</h1>
             <Progress value={progress} />
 
             <div className="hidden md:flex bg-white rounded-lg p-4 flex-col items-center gap-4">
-              <h1 className="text-2xl text-black font-bold">
+              <h1 className="text-2xl md:text-xl text-black font-bold">
                 Use o mouse para navegar através do modelo
               </h1>
 
-              <p className="text-lg text-black self-start">Para dar zoom use o scroll do mouse</p>
+              <p className="text-lg md:text-sm text-black self-start">
+                Para dar zoom use o scroll do mouse
+              </p>
 
-              <p className="text-lg text-black self-start">
+              <p className="text-lg md:text-sm text-black self-start">
                 Para girar o modelo pressione o botão esquerdo do mouse e movimente mouse
               </p>
 
-              <p className="text-lg text-black self-start">
+              <p className="text-lg md:text-sm text-black self-start">
                 Para navegar através do modelo pressione o botão direito e movimente o mouse
               </p>
             </div>

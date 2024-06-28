0e750bd0e438db1b473d484abe17e20a7d7f306a
Diego Ramos <diegorbrito9@gmail.com>
2024-06-28 11:26:41 -0300

    chore: adding modal

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -7,9 +7,7 @@ export function ProgressBar() {
 
   useEffect(() => {
     if (progress < 100) {
-      const timer = setTimeout(() => {
-        setProgress((prevState) => prevState + 10);
-      }, 1900);
+      const timer = setTimeout(() => setProgress((prevState) => prevState + 10), 1900);
 
       return () => clearTimeout(timer);
     }
@@ -21,9 +19,25 @@ export function ProgressBar() {
     <>
       {!loadingIsCompleted && (
         <section className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="flex flex-col w-1/2 gap-4">
-            <h1 className="text-4xl text-white">Carregando ...</h1>
+          <div className="flex flex-col w-1/2 gap-4 items-center">
+            <h1 className="text-4xl text-white font-bold">Carregando ...</h1>
             <Progress value={progress} />
+
+            <div className="bg-white rounded-lg p-4 flex flex-col items-center gap-4">
+              <h1 className="text-2xl text-black font-bold">
+                Use o mouse para navegar através do modelo
+              </h1>
+
+              <p className="text-lg text-black self-start">Para dar zoom use o scroll do mouse</p>
+
+              <p className="text-lg text-black self-start">
+                Para girar o modelo pressione o botão esquerdo do mouse e movimente mouse
+              </p>
+
+              <p className="text-lg text-black self-start">
+                Para navegar através do modelo pressione o botão direito e movimente o mouse
+              </p>
+            </div>
           </div>
         </section>
       )}
